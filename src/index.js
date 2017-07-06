36ff63562453b41d97093ff87d5a37419cfc0a03
vpolulikh <v.polulikh@relevant.software>
2017-07-06 19:30:41 +0300

    remove idea 1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Router, hashHistory } from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
+// registerServiceWorker();
