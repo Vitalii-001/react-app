f0ab1f8c2b91a8bef1898b4486d5d18fac9e1135
vpolulikh <v.polulikh@relevant.software>
2017-07-31 16:12:31 +0300

    add tslint json

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import * as React from 'react';
-import { connect } from 'react-redux';
+import * as React from "react";
+import { connect } from "react-redux";
 
-import PhotoList from './components/PhotoList/PhotoList'
-import Header from './components/Layout/Header/Header';
-import PhotoView from './components/PhotoList/PhotoView/PhotoView';
+import PhotoList from "./components/PhotoList/PhotoList"
+import Header from "./components/Layout/Header/Header";
+import PhotoView from "./components/PhotoList/PhotoView/PhotoView";
 
 class App extends React.Component<any, any> {
-    render() {
+    public render() {
         return (
             <div>
                 <Header/>
                 <PhotoList/>
             </div>
-        )
+        );
     }
 }
-function mapStateToProps (state: any, ownProps: any) {
+function mapStateToProps(state: any, ownProps: any) {
     return {
         store: state,
         ownProps
-    }
+    };
 }
 
 export default connect(mapStateToProps)(App);
